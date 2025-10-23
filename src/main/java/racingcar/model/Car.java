aaa791fa4abb262cc0984746563028f4dcab1efd
haeyoon1 <hynam1220@naver.com>
2025-10-23 20:32:53 +0900

    refactor: Car 클래스 상수들 상수화 및 검증 로직 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,22 +1,33 @@
 package racingcar.model;
 
+import racingcar.exception.Message;
 import racingcar.util.RandomGenerator;
 
 public class Car {
+
+    private static final int MOVE_STANDARD = 4;
+    private static final int MOVE_DISTANCE = 1;
 
     private String name;
     private int position = 0;
 
     public Car(String name) {
+        validateCarName(name);
         this.name = name;
+    }
+
+    private void validateCarName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException(Message.INVALID_CAR_INPUT.getValue());
+        }
     }
 
     public void moveCar() {
         int number = RandomGenerator.randomGenerator();
-        if (number < 4) {
+        if (number < MOVE_STANDARD) {
             return;
         }
-        position += 1;
+        position += MOVE_DISTANCE;
     }
 
     public String getName() {
