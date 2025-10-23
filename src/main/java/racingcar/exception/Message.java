c0b7d281108307147988b5ed69527beef6c2a942
haeyoon1 <hynam1220@naver.com>
2025-10-24 04:03:06 +0900

    refactor: 에러 메세지 수정

--- src/main/java/racingcar/exception/Message.java
+++ src/main/java/racingcar/exception/Message.java
@@ -1,14 +1,16 @@
 package racingcar.exception;
 
 public enum Message {
-    INVALID_CAR_INPUT("자동차 이름을 하나 이상 입력해주세요."),
-    ROUND_RANGE_ERROR("시도할 횟수는 1 이상이어야 합니다."),
-    ROUND_INPUT_EMPTY("시도할 횟수를 입력해주세요."),
+    EMPTY_CAR_INPUT("자동차 이름이 입력되지 않았습니다."),
+    INVALID_CAR_INPUT("자동차 이름을 두 개 이상 입력해주세요."),
 
     CAR_NAME_MAX_LENGTH_LIMIT("자동차 이름은 5자 이하입니다."),
-    CAR_NAME_MIN_LENGTH_LIMIT("자동차 이름은 1자 이상입니다."),
+    CAR_NAME_MIN_LENGTH_LIMIT("자동차 이름은 공백이 될 수 없습니다."),
 
-    WINNING_CAR_LIST_EMPTY("우승자는 한 명 이상입니다.");
+    DUPLICATE_CAR_NAME("중복된 자동차 이름이 존재합니다."),
+
+    WINNING_CAR_LIST_EMPTY("우승자는 한 명 이상입니다.")
+    ;
 
     private final String message;
 
