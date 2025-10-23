314e351d33b847a665eaf075aeaca012260c4707
haeyoon1 <hynam1220@naver.com>
2025-10-23 16:15:26 +0900

    feat: 결과 값을 출력하는 InputView 생성

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.model.Car;
+
+public class OutputView {
+
+    public static void printGameResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println("");
+    }
+
+    public static void printGameWinner(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+}
