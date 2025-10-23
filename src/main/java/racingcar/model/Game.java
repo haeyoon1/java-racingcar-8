92985e9509314a4d5decc91e00069e5bc293fc14
haeyoon1 <hynam1220@naver.com>
2025-10-23 21:55:04 +0900

    refactor: Game 클래스 메서드 분리 - initializeCars: Cars 객체 생성 메서드 생성 - playGame: 게임 진행 메서드 생성

--- src/main/java/racingcar/model/Game.java
+++ src/main/java/racingcar/model/Game.java
@@ -6,20 +6,26 @@ import racingcar.view.OutputView;
 public class Game {
 
     public Cars carRacing(String inputCars, int inputRounds) {
-        Cars cars = new Cars(
+        Cars cars = initializeCars(inputCars);
+        playGame(cars, inputRounds);
+        return cars.calculateWinners();
+    }
+
+    private Cars initializeCars(String inputCars) {
+        return new Cars(
             InputParser.splitCarNames(inputCars)
                 .stream()
                 .map(Car::new)
                 .toList()
         );
+    }
 
+    private void playGame(Cars cars, int inputRounds) {
         for (int i = 0; i < inputRounds; i++) {
             for (Car car : cars.getCars()) {
                 car.moveCar();
             }
             OutputView.printGameResult(cars);
         }
-
-        return cars.calculateWinners();
     }
 }
