0226de15a96f15e09db604d354feea36bc065312
haeyoon1 <hynam1220@naver.com>
2025-10-27 22:52:49 +0900

    refactor: Round의 불필요한 검증 로직 제거 - 비즈니스 사항 검증 로직을 제외한 검증 로직 제거 - 입력 시 시도 횟수의 자료형 String -> int

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -2,6 +2,7 @@ package racingcar.controller;
 
 import racingcar.model.Cars;
 import racingcar.model.Round;
+import racingcar.model.WinningCars;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -10,18 +11,18 @@ public class RacingCarController {
     public void run() {
         String inputCarNames = InputView.inputCars();
         Cars cars = Cars.fromInput(inputCarNames);
-        String inputRounds = InputView.inputRound();
-        Round round = new Round(inputRounds);
+        int inputRounds = InputView.inputRound();
+        Round rounds = new Round(inputRounds);
 
         OutputView.printGameResultMessage();
-        startRacing(cars, round);
+        startRacing(cars, rounds);
 
         Cars winners = cars.calculateWinners();
         OutputView.printGameWinner(winners);
     }
 
     private void startRacing(Cars cars, Round inputRounds) {
-        int rounds = inputRounds.getRound();
+        int rounds = inputRounds.value();
 
         for (int i = 0; i < rounds; i++) {
             cars.moveAll();
