405987f5097dd08474aeb506e050d8511d9da0d0
haeyoon1 <hynam1220@naver.com>
2025-10-27 22:50:25 +0900

    refactor: 잘못된 에러 메세지 문구 수정

--- src/main/java/racingcar/exception/Message.java
+++ src/main/java/racingcar/exception/Message.java
@@ -11,7 +11,7 @@ public enum Message {
 
     EMPTY_ROUND_INPUT("경주를 시도할 횟수가 입력되지 않았습니다."),
     ROUND_NOT_NUMBER("시도할 횟수는 정수로 입력해야 합니다."),
-    INVALID_ROUND_INPUT("시도할 횟수는 0 이상입니다."),
+    INVALID_ROUND_INPUT("시도할 횟수는 1 이상입니다."),
 
     WINNING_CAR_LIST_EMPTY("우승자는 한 명 이상입니다."),
     ;
